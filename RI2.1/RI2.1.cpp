36997e5cd45679bee5fe06f9eb44cf5f25d65f7f
unknown <vova.vysot2.0@gmail.com>
2022-09-29 11:39:36 +0300

    first master commit

---
+++ RI2.1/RI2.1.cpp
@@ -0,0 +1,26 @@
+﻿// Lab_02.cpp 
+// < Висоцький Володимир > 
+// Лабораторна робота № 2. 
+// Лінійні програми. 
+// Варіант 1
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+   double Pi = 4 * atan(1.); // число пі
+   double x;
+   double z1;
+   // double z2;
+
+   cout << "x = ";
+   cin >> x;
+   z1 = 2 * (sin(3 * Pi - 2 * x) * sin(3 * Pi - 2 * x)) * (cos(5 * Pi + 2 * x) * cos(5 * Pi + 2 * x));
+   //z2 = 1. / 4 - (1. / 4 * sin(5. / 2 * Pi - 8 * x));
+
+   cout << "z1 = " << z1 << endl;
+   //cout << "z2 = " << z2 << endl;
+   return 0;
+}
